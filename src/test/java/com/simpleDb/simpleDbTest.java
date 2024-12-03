825b3036cbf1492c7ca8351a17c83ffe6cbe2c05
wjwj910 <wjwj910@gmail.com>
2024-12-03 15:26:48 +0900

    create simpleDb class, R

---
+++ src/test/java/com/simpleDb/simpleDbTest.java
@@ -0,0 +1,13 @@
+package com.simpleDb;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class simpleDbTest {
+    private static SimpleDb simpleDb;
+
+    @BeforeAll
+    public static void beforeAll() {
+        simpleDb = new SimpleDb("localhost", "root", "1122", "simpleDb__test");
+
+    }
+}
