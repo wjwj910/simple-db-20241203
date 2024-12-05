11a6950a15ff7f9c8fee42f0c0087d52dd4573e2
wjwj910 <wjwj910@gmail.com>
2024-12-05 12:27:24 +0900

    insert, r

--- src/test/java/com/simpleDb/simpleDbTest.java
+++ src/test/java/com/simpleDb/simpleDbTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.*;
 
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class simpleDbTest {
     private static SimpleDb simpleDb;
 
     @BeforeAll
     public static void beforeAll() {
         simpleDb = new SimpleDb("localhost", "root", "1122", "simpleDb__test");
-
+        //simpleDb.setDevMode(true);
         createArticleTable();
 
     }
@@ -62,10 +64,35 @@ public class simpleDbTest {
         simpleDb.run("TRUNCATE article");
     }
 
+
+
     @Test
     @DisplayName("데이터베이스 연결 테스트")
     public void t000() {
 
+    }
+
+    @Test
+    @DisplayName("insert")
+    public void t001() {
+        Sql sql = simpleDb.genSql();
+        /*
+        == rawSql ==
+        INSERT INTO article
+        SET createdDate = NOW() ,
+        modifiedDate = NOW() ,
+        title = '제목 new' ,
+        body = '내용 new'
+        */
+        sql.append("INSERT INTO article")
+                .append("SET createdDate = NOW()")
+                .append(", modifiedDate = NOW()")
+                .append(", title = ?", "제목 new")
+                .append(", body = ?", "내용 new");
+
+        long newId = sql.insert(); // AUTO_INCREMENT 에 의해서 생성된 주키 리턴
+
+        assertThat(newId).isGreaterThan(0);
     }
 
 }
