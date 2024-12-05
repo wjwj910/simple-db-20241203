d534655e405719b91dd20c5b0bfcb276dd27d2b5
wjwj910 <wjwj910@gmail.com>
2024-12-05 14:26:31 +0900

    selectLong, r

--- src/test/java/com/simpleDb/simpleDbTest.java
+++ src/test/java/com/simpleDb/simpleDbTest.java
@@ -216,4 +216,23 @@ public class simpleDbTest {
         assertThat(diff).isLessThanOrEqualTo(1L);
     }
 
+    @Test
+    @DisplayName("selectLong")
+    public void t007() {
+        Sql sql = simpleDb.genSql();
+        /*
+        == rawSql ==
+        SELECT id
+        FROM article
+        WHERE id = 1
+        */
+        sql.append("SELECT id")
+                .append("FROM article")
+                .append("WHERE id = 1");
+
+        Long id = sql.selectLong();
+
+        assertThat(id).isEqualTo(1);
+    }
+
 }
