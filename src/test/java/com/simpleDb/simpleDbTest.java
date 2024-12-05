7bdfb2654b004dbab9890bf600bb68590543d9fb
wjwj910 <wjwj910@gmail.com>
2024-12-05 14:24:11 +0900

    selectDatetime, r

--- src/test/java/com/simpleDb/simpleDbTest.java
+++ src/test/java/com/simpleDb/simpleDbTest.java
@@ -3,6 +3,7 @@ package com.simpleDb;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -197,4 +198,22 @@ public class simpleDbTest {
         assertThat(articleRow.get("modifiedDate")).isNotNull();
         assertThat(articleRow.get("isBlind")).isEqualTo(false);
     }
+
+    @Test
+    @DisplayName("selectDatetime")
+    public void t006() {
+        Sql sql = simpleDb.genSql();
+        /*
+        == rawSql ==
+        SELECT NOW()
+        */
+        sql.append("SELECT NOW()");
+
+        LocalDateTime datetime = sql.selectDatetime();
+
+        long diff = ChronoUnit.SECONDS.between(datetime, LocalDateTime.now());
+
+        assertThat(diff).isLessThanOrEqualTo(1L);
+    }
+
 }
