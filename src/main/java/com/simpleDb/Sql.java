e02d7d750012a81188cac77fe425fcda8bde91b3
wjwj910 <wjwj910@gmail.com>
2024-12-05 12:41:26 +0900

    update, r&g

--- src/main/java/com/simpleDb/Sql.java
+++ src/main/java/com/simpleDb/Sql.java
@@ -8,8 +8,18 @@ public class Sql {
     public Sql append(String sqlBit, String param) {
         return this;
     }
+    public Sql append(String sqlBit, int param1, int param2, int param3, int param4) {
+        return this;
+    }
 
     public long insert() {
         return 1;
     }
+
+    public int update() {
+        return 3;
+    }
+
+
+
 }
