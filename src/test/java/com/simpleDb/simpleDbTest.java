99c00177bb7eecc3f4d1c823ba2a627b32148267
wjwj910 <wjwj910@gmail.com>
2024-12-05 14:30:32 +0900

    selectBoolean 2nd, r

--- src/test/java/com/simpleDb/simpleDbTest.java
+++ src/test/java/com/simpleDb/simpleDbTest.java
@@ -272,4 +272,19 @@ public class simpleDbTest {
 
         assertThat(isBlind).isEqualTo(false);
     }
+
+    @Test
+    @DisplayName("selectBoolean, 2nd")
+    public void t010() {
+        Sql sql = simpleDb.genSql();
+        /*
+        == rawSql ==
+        SELECT 1 = 1
+        */
+        sql.append("SELECT 1 = 1");
+
+        Boolean isBlind = sql.selectBoolean();
+
+        assertThat(isBlind).isEqualTo(true);
+    }
 }
